f85f4d3c6c742d20f99765c42c2f8383943316fc
Nazarii <nazarii@starlink.ua>
2023-09-12 00:36:14 +0300

    Refactor 8

--- 8.go
+++ 8.go
@@ -7,50 +7,28 @@ import (
 
 func main() {
 	var (
-		n         int64
-		nF        float64
-		nDiff     float64
-		match     int64
-		matchF    float64
-		SqrtN     float64
-		SqrtClear float64
+		n     int64
+		nDiff float64
+		sqrtN float64
 	)
 
 	_, _ = fmt.Scan(&n)
 
-	match = 1
-	nF = float64(n)
-	SqrtN = math.Sqrt(nF)
-	SqrtClear = math.Pow(math.Trunc(SqrtN), 2)
-	nDiff = nF - SqrtClear
+	sqrtN = math.Sqrt(float64(n))
+	nDiff = float64(n) - math.Pow(math.Trunc(sqrtN), 2)
 
-	if n == 1 {
-		fmt.Println(match * 4)
-	} else if SqrtN-math.Trunc(SqrtN) < 1e-5 {
-		matchF = float64(match)
-		matchF = SqrtN * 2 * (SqrtN + 1)
-		match = int64(matchF)
-		fmt.Println(match)
-	} else if n < 4 && n > 1 {
-		match = match*4 + (n-1)*3
-		fmt.Println(match)
-	} else if nDiff == 1 && n > 4 {
-		matchF = float64(match)
-		matchF = nDiff*3 + math.Trunc(SqrtN)*2*(math.Trunc(SqrtN)+1)
-		match = int64(matchF)
-		fmt.Println(match)
-	} else if nDiff > 1 && nDiff <= math.Trunc(SqrtN) && n > 4 {
-		matchF = float64(match)
-		matchF = math.Trunc(SqrtN)*2*(math.Trunc(SqrtN)+1) + 3 + (nDiff-1)*2
-		match = int64(matchF)
-		fmt.Println(match)
-	} else if nDiff > math.Trunc(SqrtN) && n > 4 {
-		matchF = float64(match)
-		matchF = math.Trunc(SqrtN)*2*(math.Trunc(SqrtN)+1) + 6 + (nDiff-2)*2
-		match = int64(matchF)
-		fmt.Println(match)
-
-	} else {
-
+	switch {
+	case n == 1:
+		fmt.Println(4)
+	case sqrtN-math.Trunc(sqrtN) < 1e-5:
+		fmt.Println(int64(sqrtN * 2 * (sqrtN + 1)))
+	case n < 4 && n > 1:
+		fmt.Println(4 + (n-1)*3)
+	case nDiff == 1 && n > 4:
+		fmt.Println(int64(nDiff*3 + math.Trunc(sqrtN)*2*(math.Trunc(sqrtN)+1)))
+	case nDiff > 1 && nDiff <= math.Trunc(sqrtN) && n > 4:
+		fmt.Println(int64(math.Trunc(sqrtN)*2*(math.Trunc(sqrtN)+1) + 3 + (nDiff-1)*2))
+	case nDiff > math.Trunc(sqrtN) && n > 4:
+		fmt.Println(int64(math.Trunc(sqrtN)*2*(math.Trunc(sqrtN)+1) + 6 + (nDiff-2)*2))
 	}
 }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Problem conditions
 * [Number 3](https://www.eolymp.com/en/problems/3)
 * [Number 7](https://www.eolymp.com/en/problems/7)
-* [Number 8](https://www.eolymp.com/en/problems/8)
+* [Number 8](https://basecamp.eolymp.com/problems/8)
 * [Number 9](https://www.eolymp.com/en/problems/9)
 * [Gap in Primes](https://www.codewars.com/kata/561e9c843a2ef5a40c0000a4)
 * [Not very secure](https://www.codewars.com/kata/526dbd6c8c0eb53254000110)
